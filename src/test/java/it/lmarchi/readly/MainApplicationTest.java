7c96853b7bdd886267423c1769b256856d3b21d7
Luca Marchi <luca.marchi91@gmail.com>
2022-04-23 16:36:33 +0200

    Initial project base

---
+++ src/test/java/it/lmarchi/readly/MainApplicationTest.java
@@ -0,0 +1,10 @@
+package it.lmarchi.readly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+final class MainApplicationTest {
+  @Test
+  void contextLoads() {}
+}
